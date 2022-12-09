c687ea87fe1b02bf6576dc41f91b52f36cbdd3a9
Abigiya <abiyamm91@gmail.com>
2022-12-08 16:09:42 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
  * main  - Entry
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
+
 int main (void)
 {
-     	puts("\"Programming is like building a multilingual puzzle");
+
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
+
 }	
