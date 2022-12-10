18eedb81d1cc66a3f6ea65f9fd89300bb59df28c
Abigiya <abiyamm91@gmail.com>
2022-12-10 15:46:56 -0800

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,19 +10,23 @@ int main(void)
 {
 	int p, q;
 
-	for (p = 0; p <= 98; p++)
+	for (p = 0; p <= 100; p++)
 	{
-	for (q = p + 1; q <= 98; q++)
+	for (q = 0; q <= 100; q++)
+	{
+	if (p < q && p != q)
 	{
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
 	putchar(' ');
 	putchar((q / 10) + '0');
 	putchar((q % 10) + '0');
-	if (p == 98 && q == 99)
-	continue;
+	if (!(p == 98 && q == 99))
+	{
 	putchar(',');
 	putchar(' ');
+	}
+	}
 	}
 	}
 	putchar('\n');
