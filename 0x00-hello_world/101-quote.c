5b077bfd6cb446e52b63c13d1d913df3e9d16454
Abigiya <abiyamm91@gmail.com>
2022-12-10 17:45:44 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (success)
+ */
 int main(void)
-
 {
-
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-
-
-			return (1);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
 }
