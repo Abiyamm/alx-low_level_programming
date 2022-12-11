2223c81bd44b3b9ab5dfac47087ac05520316937
Abigiya <abiyamm91@gmail.com>
2022-12-10 16:29:04 -0800

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (success)
  */
 int main(void)
+
 {
 	int p, q;
 
-	for (p = 0; p <= 100; p++)
+	for (p = 0; p <= 98; p++)
 	{
-	for (q = 0; q <= 100; q++)
-	{
-	if (p < q && p != q)
+	for (q = p + 1; q <= 99; q++)
 	{
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
 	putchar(' ');
 	putchar((q / 10) + '0');
 	putchar((q % 10) + '0');
-	if (!(p == 98 && q == 99))
-	{
+	if (p == 98 && q == 99)
+	continue;
 	putchar(',');
 	putchar(' ');
-	}
-	}
 	}
 	}
 	putchar('\n');
