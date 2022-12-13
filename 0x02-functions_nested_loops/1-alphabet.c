1959106591a657639d897cd1f2d585b3e08f1653
Abigiya <abiyamm91@gmail.com>
2022-12-13 14:02:10 -0800

    Task 1. print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alpha in lowercasse
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+}
