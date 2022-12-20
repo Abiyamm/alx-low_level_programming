9b0d64a9472f0fc74811d3f911ae9efe46608583
Abigiya <abiyamm91@gmail.com>
2022-12-19 16:05:12 -0800

    Task 8 print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_array -> print array
+ * @a: input array
+ * @n: input n elements
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n, i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf('\n');
+}
