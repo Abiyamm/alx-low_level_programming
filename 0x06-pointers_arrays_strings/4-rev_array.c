932ff135634845c46d8b81f50e8d9c6bc10f0dcf
Abigiya <abiyamm91@gmail.com>
2022-12-22 05:37:42 -0800

    Task 4 rev_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,31 +1,25 @@
 #include "main.h"
-#include <string.h>
+#include <stdio.h>
 
 /**
  *reverse_array -> reversing an array
- *@a: array a
- *@n: an element of an array
+ *@a: an array of integer
+ *@n: the number of element swap
+ *Return: nothing
  */
 void reverse_array(int *a, int n)
 {
 	int i;
 
-	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-
 	i = 0;
 	while (i < n)
 	{
-	if (i != 0)
-	{
-		printf(", ");
-	}
-	printf("%d", a[i]);
+		if (i != 0)
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
 		i++;
 	}
 	printf("\n");
-	print_array(a, sizeof(a) / sizeof(int));
-	reverse_array(a, sizeof(a) / sizeof(int));
-	print_array(a, sizeof(a) / sizeof(int));
-	return (0);
 }
-
