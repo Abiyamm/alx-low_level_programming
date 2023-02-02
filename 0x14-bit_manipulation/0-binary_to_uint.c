73f9bc8bf62d1ab39773911abb62bd663bc7eea3
Abigiya <abiyamm91@gmail.com>
2023-02-02 13:10:26 -0800

    Crackme3

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - program startup
+ * @b: char being evaluated
+ *
+ * Description: converts a binary number to an unsigned int
+ * Return: return (0) is the required function signature
+ */
+
