49524de308b8a46364251dce5dbc96d2da7b0d7c
Abigiya <abiyamm91@gmail.com>
2023-02-02 13:39:22 -0800

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * binary_to_uint - program startup
@@ -7,4 +8,26 @@
  * Description: converts a binary number to an unsigned int
  * Return: return (0) is the required function signature
  */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int x;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (x = 0; b[x] != '\0'; x++)
+	{
+		if (b[x] == '0' || b[x] == '1')
+		{
+			num <<= 1;
+			num += b[x] - '0';
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (num);
+}
